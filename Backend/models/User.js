25cbf74ce25211e8dd9a87e2b92851311e223461
Mlowegene <genemlowe@gmail.com>
2024-09-19 10:04:04 +0300

    fix typos

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, required: true},
     password: {type: String, required: true},
     role: {type: String,enum: ['admin', 'employee'], required: true},
-    profileImage: {type: string},
+    profileImage: {type: String},
     createAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now}
 })
