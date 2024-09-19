827e9ba7bd6c7acd6e1cd4555fcb52ff5beddc6d
Mlowegene <genemlowe@gmail.com>
2024-09-19 15:36:11 +0300

    feat: added admin overview page

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom"
 import { useAuth } from "../context/authContext"
 import AdminSidebar from "../components/dashboard/AdminSidebar"
 import Navbar from "../components/dashboard/Navbar"
+import AdminSummary from "../components/dashboard/AdminSummary"
 
 function AdminDashboard() {
   const {user} = useAuth()
@@ -11,6 +12,7 @@ function AdminDashboard() {
       <AdminSidebar />
       <div className="flex-1 ml-64 bg-gray-100 h-screen">
         <Navbar />
+        <AdminSummary />
       </div>
     </div>
   )
