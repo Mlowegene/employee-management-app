12034bf3e69664adb999fae29f7210e285f7d900
Mlowegene <genemlowe@gmail.com>
2024-09-20 10:44:08 +0300

    created department routes(API)

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './database.js';
 import authRouter from './routes/auth.js'
+import departmentRouter from './routes/department.js'
 
 const app = express()
 
@@ -9,6 +10,7 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter)
 
 
 app.listen(process.env.PORT, ()=> {
