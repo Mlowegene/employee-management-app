dda745f2733d73591cc92c06648b9a45ce78e342
Mlowegene <genemlowe@gmail.com>
2024-10-01 11:31:36 +0300

    fix typo

--- Backend/models/Department.js
+++ Backend/models/Department.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const departmentSchema = new mongoose.Schema({
-    dep_name: {type: string, required: true },
-    description: {type: dtring},
+    dep_name: {type: String, required: true },
+    description: {type: String},
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now}
 })
