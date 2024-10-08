d1c05f933793fe10e2463cfbf9d3841706d1a49a
Mlowegene <genemlowe@gmail.com>
2024-10-08 11:37:14 +0300

    feat: added logout feature

--- Frontend/src/components/dashboard/Navbar.jsx
+++ Frontend/src/components/dashboard/Navbar.jsx
@@ -3,11 +3,11 @@ import { useAuth } from "../../context/authContext"
 useAuth
 function Navbar() {
 
-    const {user} = useAuth()
+    const {user, Logout} = useAuth()
   return (
     <div className="flex items-center text-white justify-between h-12 bg-blue-600 px-5">
       <p>Welcome {user.name}</p>
-      <button className="px-4 py-1 bg-blue-700 hover:bg-blue-800">logout</button>
+      <button className="px-4 py-1 bg-blue-700 hover:bg-blue-800" onClick={Logout}>logout</button>
     </div>
   ) 
 }
