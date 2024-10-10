d62fe667d5c56d95461ef6c7b41fa7aed827c36b
Mlowegene <genemlowe@gmail.com>
2024-10-10 11:57:57 +0300

    feat: created employee component

--- Frontend/src/components/employee/List.jsx
+++ Frontend/src/components/employee/List.jsx
@@ -1,15 +1,63 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom"
+import DataTable from 'react-data-table-component'
+import axios from "axios";
+import { columns, EmployeeButtons } from "../../utils/EmployeeHelpers";
 
 
 function List() {
+
+    const [employees, setEmployees] = useState({})
+    const [empLoading, setEmpLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchEmployees = async () => {
+            setEmpLoading(true)
+          try {
+              const response = await axios.get('http://localhost:5000/api/employee', {
+                  headers: {
+                      'Authorization': `Bearer ${localStorage.getItem('token')}`
+                  }
+              })
+              
+              if(response.data.success) {
+                  let sno = 1;
+                  const data = await response.data.employees.map((emp)=> (
+                      {
+                          _id: emp._id,
+                          sno: sno++,
+                          dep_name: emp.department.dep_name,
+                          name: emp.userId.name,
+                          dob: new Date(emp.dob).toLocaleDateString(),
+                          profileImage: <img width={40} className="rounded-full" src={`http://localhost:5000/${emp.userId.profileImage}`} />,
+                          action: (<EmployeeButtons Id={emp._id} />)
+                      }
+                  ))
+                  setEmployees(data)
+              }
+          } catch (error) {
+              if(error.response && !error.response.data.success) {
+                  alert(error.response.data.error)
+              }
+          }finally{
+            setEmpLoading(false)
+          }
+        };
+    
+        fetchEmployees()
+      }, [])
+
   return (
     <div className="p-5">
       <div className='text-center'>
         <h3 className='text-2xl font-bold'>Manage Employee</h3>
       </div>
       <div className='flex justify-between items-center'>
-        <input type="text" placeholder="Search by dept name..." className='px-4 py-0.5 border' />
+        <input type="text" placeholder="Search by employee name..." className='px-4 py-0.5 border' />
         <Link to="/admin-dashboard/add-employee" className='px-4 py-1 bg-teal-400 rounded text-white'>Add New Employee</Link>
+      </div>
+      <div>
+        <DataTable columns={columns} data={employees} />
       </div>
     </div>
   )
--- Frontend/src/utils/EmployeeHelpers.jsx
+++ Frontend/src/utils/EmployeeHelpers.jsx
@@ -1,5 +1,42 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
+
+export const columns = [
+    {
+        name: "S No",
+        selector: (row) => row.sno,
+        width: "90px"
+    },
+    {
+        name: "Name",
+        selector: (row) => row.name,
+        sortable: true,
+        width: "120px"
+    },
+    {
+        name: "Image",
+        selector: (row) => row.profileImage,
+        width: "100px"
+    },
+    {
+        name: "Department",
+        selector: (row) => row.dep_name,
+        width: "130px"
+    },
+    {
+        name: "DOB",
+        selector: (row) => row.dob,
+        sortable: true,
+        width: "130px"
+    },
+    {
+        name: "Action",
+        selector: (row) => row.action,
+        center: "true"
+    }
+
+]
 
 export const fetchDepartment = async () => {
     let departments;
@@ -19,3 +56,35 @@ export const fetchDepartment = async () => {
     }
     return departments;
   };
+
+
+  export const EmployeeButtons = ({Id}) => {
+    const navigate = useNavigate();
+
+
+    return(
+        <div className="flex space-x-3">
+            <button 
+            className="px-3 py-1 bg-teal-600 text-white"
+            onClick={() => navigate(`/admin-dashboard/department/${Id}`)}
+            >
+                View
+            </button>
+            <button 
+            className="px-3 py-1 bg-blue-600 text-white"
+            >
+                Edit
+            </button>
+            <button 
+            className="px-3 py-1 bg-yellow-600 text-white"
+            >
+                Salary
+            </button>
+            <button 
+            className="px-3 py-1 bg-red-600 text-white"
+            >
+                Leave
+            </button>
+        </div>
+    )
+}
