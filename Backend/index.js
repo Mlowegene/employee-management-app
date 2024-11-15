9f0e9e5e2c81525c71551a2403bd350c7d2d1eff
Mlowegene <genemlowe@gmail.com>
2024-11-15 12:48:31 +0300

    feat:(backend) add setting routes

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,8 @@ import authRouter from './routes/auth.js'
 import departmentRouter from './routes/department.js'
 import employeeRouter from './routes/employee.js'
 import salaryRouter from './routes/salary.js'
+import leaveRouter from './routes/leaves.js'
+import settingRouter from './routes/setting.js'
 
 const app = express()
 
@@ -16,6 +18,9 @@ app.use('/api/auth', authRouter)
 app.use('/api/department', departmentRouter)
 app.use('/api/employee', employeeRouter)
 app.use('/api/salary', salaryRouter)
+app.use('/api/leave', leaveRouter)
+app.use('/api/setting', settingRouter)
+
 
 app.listen(process.env.PORT, ()=> {
     console.log(`server listening on port number ${process.env.PORT}`);
