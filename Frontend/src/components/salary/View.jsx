15a5cfad457364539251b0cbbca96fdcee6932ad
Mlowegene <genemlowe@gmail.com>
2024-11-26 11:02:39 +0300

    fix: (frontend) improve some changes on salary view

--- Frontend/src/components/salary/View.jsx
+++ Frontend/src/components/salary/View.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useState, useEffect } from "react"
 import { useParams} from "react-router-dom"
+import { useAuth } from "../../context/authContext";
 
 
 function View() {
@@ -9,11 +10,13 @@ const [salaries, setSalaries] = useState(null);
 const [filteredSalaries, setFilteredSalaries] = useState(null)
 const {id} = useParams()
 
+const {user} = useAuth()
+
 let sno = 1;
 
 const fetchSalaries = async () => {
     try {
-       const response = await axios.get(`http://localhost:5000/api/salary/${id}`, {
+       const response = await axios.get(`http://localhost:5000/api/salary/${id}/${user.role}`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
@@ -66,6 +69,7 @@ const fetchSalaries = async () => {
             {filterSalaries.length > 0 ? (
                 <table className="w-full text-sm text-left text-gray-500">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-200 border border-gray-200">
+                        <tr>
                         <th className="px-6 py-3">SNO</th>
                         <th className="px-6 py-3">Emp ID</th>
                         <th className="px-6 py-3">Salary</th>
@@ -73,6 +77,7 @@ const fetchSalaries = async () => {
                         <th className="px-6 py-3">Deduction</th>
                         <th className="px-6 py-3">Total</th>
                         <th className="px-6 py-3">Pay Date</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {filteredSalaries.map((salary)=> (
